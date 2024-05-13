28ac1c439c08a31acec49ca54aa5fd27136529b5
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2024-05-13 13:27:18 -0600

    fixed project layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
 import { ProjectCard } from './ProjectCard';
-import projImg1 from '../assets/img/coffeeShopRock2.jpg';
+import projImg1 from '../assets/img/coffeeShopRock5.jpg';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
 import projImg4 from '../assets/img/jQueryQuiz3.jpg';
