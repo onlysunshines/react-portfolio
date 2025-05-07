dcf623aab53386434238a01922b68296cc260088
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2025-05-07 15:21:36 -0600

    getting closer

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -3,7 +3,7 @@ import { Newsletter } from './Newsletter';
 
 export const MailchimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
-    console.log(Newsletter);
+    //console.log(Newsletter);
 
     return (
         <>
