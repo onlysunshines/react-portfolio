4ae0959d7750a2c68d460538a32a745e48b5bc4a
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2025-05-17 13:54:16 -0600

    get chimpy?

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -27,7 +27,7 @@ export const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("https://react-portfolio-vzy0.onrender.com/connect", {
+        let response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/contact`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
