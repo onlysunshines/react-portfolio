0ad6bc8fb3f2e8a1a505ce9f2ef62817593a99b6
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2025-05-17 18:46:02 -0600

    certs

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Certs } from './components/Certs';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-// import { Newsletter } from './components/Newsletter';
+import { Newsletter } from './components/Newsletter';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Certs />
       <Skills />
       <Projects />
       <Contact />
-      {/* <Newsletter /> */}
+      <Newsletter />
       <Footer />
     </div>
   );
---
+++ client/src/components/Certs.js
@@ -0,0 +1,19 @@
+
+
+export const Certs = () => {
+    
+    return (
+        <section className="cert" id="certs">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>Certifications</h2>
+                            <p>I am constantly improving my knowledge base and expanding my comfort zone.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        </div>
+                    </div>                    
+                </div>
+            </div>            
+        </section>
+    )
+}
