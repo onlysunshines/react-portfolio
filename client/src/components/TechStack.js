10517cdd8186745480c99c0e29670a6e07ef2dab
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2025-05-31 11:46:46 -0600

    new express logo

--- client/src/components/TechStack.js
+++ client/src/components/TechStack.js
@@ -2,7 +2,7 @@ import HTML5 from '../assets/img/HTML5.png';
 import CSS3 from '../assets/img/CSS3.png';
 import JS5 from '../assets/img/javascript-r.svg';
 import ReactLogo from '../assets/img/react-2.svg';
-import EX from '../assets/img/Express.png';
+import EX from '../assets/img/express-109.svg';
 import NodeJS from '../assets/img/NodeJS.png';
 import Tailscale from '../assets/img/Tailscale.png';
 import Render from '../assets/img/Render.png';
